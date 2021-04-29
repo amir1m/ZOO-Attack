fac23572a303f9fbbf10b24c92df4b9093f2b6d2
amir1m <amir_mukeri@yahoo.com>
2021-04-29 20:36:45 +0530

    model.load_model(restore + "/mnist.h5")

--- setup_mnist.py
+++ setup_mnist.py
@@ -93,7 +93,7 @@ class MNISTModel:
         if use_log:
             model.add(Activation('softmax'))
         if restore:
-            model.load_weights(restore + "/variables")
+            model.load_model(restore + "/mnist.h5")
 
         self.model = model
 
