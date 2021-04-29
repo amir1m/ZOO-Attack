5bf8d0fbe51d085694d29fe0210d976636aef4de
amir1m <amir_mukeri@yahoo.com>
2021-04-29 19:35:37 +0530

    Made tensorflow.compat.v1 for TF2x compatibility

--- test_all.py
+++ test_all.py
@@ -9,7 +9,9 @@
 
 import os
 import sys
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 import random
 import time
