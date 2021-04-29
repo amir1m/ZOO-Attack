b7dcc1edfe5c81ebbea42dc44411e542c102c232
amir1m <amir_mukeri@yahoo.com>
2021-04-29 21:37:49 +0530

    tensorflow.compat.v1

--- l2_attack_black.py
+++ l2_attack_black.py
@@ -9,7 +9,9 @@
 
 import sys
 import os
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 import scipy.misc
 from numba import jit
